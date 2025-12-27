106330a1b7d86483aaf9ae1b8ab204698654bc6a
dinhdat07 <dinhdatnguyen0710@gmail.com>
2025-12-27 12:29:24 +0700

    fix: handle zero loss cases in run_class_incremental function

--- main.py
+++ main.py
@@ -150,6 +150,8 @@ def run_class_incremental(cfg, device):
                     aug_feas = aug_feas / aug_feas.norm(dim=-1, keepdim=True)
                     sim_img = final_image_feas[:aug_feas.shape[0]] @ aug_feas.T
                     image_aug_loss = contrastive_loss(sim_img)
+                else:
+                    image_aug_loss = torch.tensor(0.0, device=device)
 
                 labels = [model.total_class_names[int(y)] for y in targets.tolist()]
                 texts_clip=[model.prompt_template.format(inst) for inst in labels]
@@ -172,8 +174,8 @@ def run_class_incremental(cfg, device):
                         anchor_text_loss_list.append(contrastive_loss(clip_text_feas @ anchor_text_features.T))
                     anchor_text_loss = sum(anchor_text_loss_list) / len(anchor_text_loss_list)
                 else:
-                    anchor_text_loss = 0
-                
+                    anchor_text_loss = torch.tensor(0.0, device=device)
+
                 clip_loss=cliploss(final_image_feas, clip_text_feas, model.logit_scale)
 
                 loss = clip_loss + model.lambda_img * image_aug_loss + model.lambda_txt * anchor_text_loss + loss_hinge
@@ -218,12 +220,6 @@ def run_class_incremental(cfg, device):
             inputs, targets = inputs.to(device), targets.to(device)
             with torch.no_grad():
                 outputs, _, __, ___, _pre_image_feas, raw_image_feas = model(inputs)
-                outputs = gda_output(
-                    model=model,
-                    cfg=cfg,
-                    outputs=outputs,
-                    raw_image_feas=raw_image_feas,
-                )
                 torch.nn.functional.softmax(outputs, dim=-1)
             metric_logger.add([outputs.cpu().argmax(dim=1), targets.cpu(), task_ids], subset="test")
 
